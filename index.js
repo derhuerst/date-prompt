cc78a13e271411f1eb9bf619d4d63365175ac7c3
Jannis R <mail@jannisr.de>
2016-01-24 23:13:37 +0100

    full rewrite

--- index.js
+++ index.js
@@ -1,119 +1,75 @@
-var moment =		require('moment');
-var bluebird =		require('bluebird');
-var escapes =		require('ansi-escapes');
-var lPad =			require('left-pad');
-var chalk =			require('chalk');
+var moment =   require('moment');
+var bluebird = require('bluebird');
+var esc =      require('ansi-escapes');
+var lPad =     require('left-pad');
+var chalk =    require('chalk');
 
 
 
 
 
-module.exports = {
+var digits = [
+	{unit: 'day',    method: 'day',    length: 2, format: 'ddd', offset: 1},
+	{unit: 'day',    method: 'date',   length: 2, format: 'DD'},
+	{unit: 'month',  method: 'month',  length: 2, format: 'MMM', offset: 1},
+	{unit: 'year',   method: 'year',   length: 4, format: 'YYYY'},
+	{unit: 'hour',   method: 'hour',   length: 2, format: 'HH', separator: ':'},
+	{unit: 'minute', method: 'minute', length: 2, format: 'mm', separator: ''}
+];
 
 
 
-	digits: [
-		{
-			unit:		'day',
-			method:		'day',
-			length:		2,
-			offset:		1,   // because Sunday is 0
-			format:		'ddd'
-		}, {
-			unit:		'day',
-			method:		'date',
-			length:		2,
-			format:		'DD'
-		}, {
-			unit:		'month',
-			method:		'month',
-			length:		2,
-			offset:		1,   // because January is 0
-			format:		'MMM'
-		}, {
-			unit:		'year',
-			method:		'year',
-			length:		4,
-			format:		'YYYY'
-		}, {
-			unit:		'hour',
-			method:		'hour',
-			length:		2,
-			format:		'HH',
-			separator:	':'
-		}, {
-			unit:		'minute',
-			method:		'minute',
-			length:		2,
-			format:		'mm',
-			separator:	''
-		}
-	],
+
+
+var DatePrompt = {
 
 
 
-	_m:		null,   // moment.js object
-	_i:		null,   // stdin
-	_o:		null,   // stdout
+	question: null,
+	moment:   null,   // moment.js object
+	value:    null,   // promise
 
-	_df:	null,   // deferred
-	date:	null,   // promise
+	cursor:   null,   // current index for `digits`
 
-	_d:		null,   // current index of `this.digits`
-	_p:		null,   // temporary typed value
-	_k:		null,   // key handler
-	_l:		null,   // when a number key was pressed the last time
+	stdin:    null,   // stdin
+	stdout:   null,   // stdout
+
+	// _resolve, _reject
+
+	cache:    null,   // typed be used
+	pressed:     null,   // when a number key was pressed the last time
 
 
 
 	init: function (question, options) {
-		var self = this;
 		options = options || {};
-
-		if (!question || 'string' != typeof question)
+		if ('string' !== typeof question)
 			throw new Error('`question` must be a string.');
 
-		this._q = question;
-		this._m = options.date ? moment(options.date) : moment();
-		this._i = options.stdin || process.stdin;
-		this._o = options.stdout || process.stdout;
+		this.question = question;
+		this.moment = options.value ? moment(options.value) : moment();
+		var self = this;
+		this.value = new Promise(function (_resolve, _reject) {
+			self.resolve = _resolve;
+			self.reject =  _reject;
+		});
 
-		// todo: let `DatePrompt` directly inherit from bluebird
-		this._df = bluebird.defer();
-		this.date = this._df.promise;
+		if (0 < options.cursor <= (digits.length - 1))
+			this.cursor = parseInt(options.cursor);
+		else this.cursor = 0;
 
-		this._d = 0;
-		this._p = '';
+		this.stdin = options.stdin || process.stdin;
+		this.stdin.setRawMode(true);
+		this.stdin.resume();
 
-		var _k = function (key) {
-			key = key.toString('utf8');
+		this.onKey = DatePrompt.onKey.bind(this);
+		this.stdin.on('data', this.onKey);
 
-			if(/\x1b\[+[A-Z]/.test(key)) {
-				key = key.charAt(key.length - 1);
-				if		(key === 'A')	return self.up();
-				else if	(key === 'B')	return self.down();
-				else if	(key === 'C')	return self.right();
-				else if	(key === 'D')	return self.left();
-				else if	(key === 'F')	return self.first();
-				else if	(key === 'H')	return self.last();
-			}
+		this.stdout = options.stdout || process.stdout;
+		this.stdout.write(esc.cursorHide);
 
-			else if	(key === '\u0003')	return self.abort();   // ctrl + C
-			else if	(key === '\x1b')	return self.abort();   // escape
-			else if	(key === '\r')		return self.submit();   // carriage return
-			else if	(key === '\n')		return self.submit();   // newline
-			else if	(key === '\t')		return self.right();   // tab
-
-			else if	(/[0-9]/.test(key))	return self.number(parseInt(key));   // number key
-			else if (key.length > 1)
-				for (var i = 0; i < key.length; i++) _k(key[i]);
-		};
-		this._k = _k;
-		this._i.on('data', _k);
-
-		this._i.setRawMode(true);
-		this._i.resume();
-		this._o.write(escapes.cursorHide);
+		this.cache = '';
+		this.pressed = Date.now();
 
 		this.render();
 		return this;
@@ -122,45 +78,63 @@ module.exports = {
 
 
 	abort: function () {
-		this._df.reject();
-
-		this._i.removeListener('data', this._k);
-		this._i.setRawMode(false);
-		this._i.pause();
-
-		this._o.write(escapes.eraseLine + '\r' + escapes.cursorShow);
+		this.reject();
+		this.stdin.removeListener('data', this.onKey);
+		this.stdin.setRawMode(false);
+		this.stdin.pause();
+		this.stdout.write(esc.eraseLine + '\r' + esc.cursorShow);
 	},
 
 	submit: function () {
-		this._df.resolve(this._m);
+		this.resolve(this.moment);
 
-		this._i.removeListener('data', this._k);
-		this._i.setRawMode(false);
-		this._i.pause();
+		this.stdin.removeListener('data', this.onKey);
+		this.stdin.setRawMode(false);
+		this.stdin.pause();
 
 		this.render(true);
-		this._o.write('\r\n' + escapes.cursorShow);
+		this.stdout.write('\r\n' + esc.cursorShow);
 	},
 
 
 
-	// number key handling
+	onKey: function (key) {
+		key = key.toString('utf8');
 
-	number: function (n) {
-		var now = new Date();
-		if ((now - this._l) > 1000)   // a lot of time elapsed
-			this._p = '' + n;   // reset temporary value
+		if(/\x1b\[+[A-Z]/.test(key)) {
+			key = key.charAt(key.length - 1);
+			if		(key === 'A')	return this.up();
+			else if	(key === 'B')	return this.down();
+			else if	(key === 'C')	return this.right();
+			else if	(key === 'D')	return this.left();
+			else if	(key === 'F')	return this.first();
+			else if	(key === 'H')	return this.pressed();
+		}
+
+		else if	(key === '\u0003')	return this.abort(); // ctrl + C
+		else if	(key === '\x1b')	return this.abort(); // escape
+		else if	(key === '\r')		return this.submit(); // carriage return
+		else if	(key === '\n')		return this.submit(); // newline
+		else if	(key === '\t')		return this.right(); // tab
+
+		else if	(/[0-9]/.test(key))	return this.onNumber(parseInt(key)); // number key
+	},
+
+
+
+	onNumber: function (n) {
+		var now = Date.now();
+		if ((now - this.pressed) > 1000) this.cache = '' + n; // 1s elapsed
 		else {
-			this._p += n;
-			if (this._p.length >= this.digits[this._d].length) {   // got all digits
-				var v = parseInt(this._p);
-				v -= this.digits[this._d].offset || 0;
-				this._m[this.digits[this._d].method](v);
-				this._p = '';
-				if (this._d < this.digits.length - 1) this._d++;
+			this.cache += n;
+			if (this.cache.length >= digits[this.cursor].length) { // got all digits
+				var v = this.cache - (digits[this.cursor].offset || 0);
+				this.moment[digits[this.cursor].method](v);
+				this.cache = '';
+				if (this.cursor < digits.length - 1) this.cursor++;
 			}
 		}
-		this._l = now;
+		this.pressed = now;
 		this.render();
 	},
 
@@ -169,61 +143,71 @@ module.exports = {
 	// arrow key handling
 
 	first: function () {
-		if (this._d != 0) this._p = '';
-		this._d = 0;
+		if (this.cursor !== 0) this.cache = '';
+		this.cursor = 0;
 		this.render();
 	},
-	right: function () {
-		if (this._d != this.digits.length - 1) this._p = '';
-		this._d = this.digits.length - 1;
+	last: function () {
+		if (this.cursor !== digits.length - 1) this.cache = '';
+		this.cursor = digits.length - 1;
 		this.render();
 	},
 
 	left: function () {
-		if (this._d > 0) {
-			this._d--;
-			this._p = '';
-		} else this._o.write(escapes.beep);
+		if (this.cursor > 0) {
+			this.cursor--;
+			this.cache = '';
+		} else this.stdout.write(esc.beep);
 		this.render();
 	},
 	right: function () {
-		if (this._d < this.digits.length - 1) {
-			this._d++;
-			this._p = '';
-		} else this._o.write(escapes.beep);
+		if (this.cursor < digits.length - 1) {
+			this.cursor++;
+			this.cache = '';
+		} else this.stdout.write(esc.beep);
 		this.render();
 	},
 
 	up: function () {
-		this._m.add(1, this.digits[this._d].unit);
-		this._p = '';
+		this.moment.add(1, digits[this.cursor].unit);
+		this.cache = '';
 		this.render();
 	},
 	down: function () {
-		this._m.subtract(1, this.digits[this._d].unit);
-		this._p = '';
+		this.moment.subtract(1, digits[this.cursor].unit);
+		this.cache = '';
 		this.render();
 	},
 
 
 
 	render: function (formatted) {
-		this._o.write(escapes.eraseLine + '\r' + chalk.green(formatted ? '✔' : '?') + ' ');
-		this._o.write(chalk.bold(this._q) + ' ');
+		var o = this.stdout;
+
+		o.write(esc.eraseLine + '\r' + chalk.green(formatted ? '✔' : '?') + ' ');
+		o.write(chalk.bold(this.question) + ' ');
 
 		var i, digit;
-		for (i = 0; i < this.digits.length; i++) {
-			digit = this.digits[i];
+		for (i = 0; i < digits.length; i++) {
+			digit = digits[i];
 
-			if (!formatted && i === this._d)
-				this._o.write(chalk.cyan.underline(this._m.format(digit.format)));
-			else this._o.write(this._m.format(digit.format));
+			if (!formatted && i === this.cursor)
+				o.write(chalk.cyan.underline(this.moment.format(digit.format)));
+			else o.write(this.moment.format(digit.format));
 
-			if ('string' === typeof digit.separator) this._o.write(digit.separator);
-			else this._o.write(' ');
+			o.write(('string' === typeof digit.separator) ? digit.separator : ' ');
 		}
-	},
+	}
 
 
 
 };
+
+
+
+var create = module.exports = function (question, options) {
+	var prompt = Object.create(DatePrompt);
+	prompt.init(question, options);
+	return prompt.value;
+};
+create.DatePrompt = DatePrompt;
